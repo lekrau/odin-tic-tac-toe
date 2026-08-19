70d1eb6ae1946fb5b0bb77061194383ac9b0e1c2
lekrau <206306997+lekrau@users.noreply.github.com>
2026-08-19 07:14:05 +0200

    Initialize 3x3 gameboard

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 "use strict";
 
-// - Your main goal here is to have as little global code as possible. Try tucking as much as you can inside factories.
-//   If you only need a single instance of something (e.g. the gameboard, the displayController etc.)
-//   then wrap the factory inside an IIFE (module pattern) so it cannot be reused to create additional instances.
-// - In this project, think carefully about where each bit of logic should reside.
-//   Each little piece of functionality should be able to fit in the game, player or gameboard objects.
-//   Take care to put them in “logical” places. Spending a little time brainstorming here can make your life much easier later!
-// - If you’re having trouble, Building a house from the inside out is a great article that lays out a highly applicable example
-//   both of how you might approach tackling this project as well as how you might organize and structure your code.
-
 const gameboard = (() => {
+    const rows = 3;
+    const columns = 3;
     const gameboard = [];
-    // TODO: Create array structure
+
+    for (let i = 0; i < rows; i++) {
+        gameboard[i] = [];
+        for (let j = 0; j < columns; j++) {
+            gameboard[i].push("");
+        }
+    }
 
     const getGameboard = () => gameboard;
 
