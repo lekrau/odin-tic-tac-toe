c18938d460a6168040f685c9712285c5ecb6f248
lekrau <206306997+lekrau@users.noreply.github.com>
2026-08-20 08:06:14 +0200

    Identify winning player

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const gameboard = (() => {
     const columns = 3;
     const gameboard = [];
 
+    // TODO: Put into initialize gameboard function or something, call it once and return it to make it available for the game controler
     for (let i = 0; i < rows; i++) {
         gameboard[i] = [];
         for (let j = 0; j < columns; j++) {
@@ -57,11 +58,19 @@ const game = (() => {
     };
 
     const makeMove = (row, column) => {
-        // TODO
-        // Later: Check if move is invalid (field is already occupied or out of range) -> error
+        // TODO (later): Check if move is invalid (field is already occupied or out of range) -> error
         gameboard.addMarker(row, column, activePlayer.getMarker());
         toggleActivePlayer();
-        gameOver();
+
+        if (gameOver()) {
+            // TODO: Reset game - WEITER!!!
+            // Remember special handling for ties
+            if (gameOver() === "tie") {
+                console.log("tie");
+            } else {
+                console.log(gameOver().getName(), "won");
+            }
+        }
 
         // For testing - TODO: Remove later
         console.log(gameboard.getGameboard()[0]);
@@ -73,42 +82,44 @@ const game = (() => {
         const board = gameboard.getGameboard();
 
         // TODO (later): Probably can be done more elegant
+        // The return value of the winner evaluates to true
         if (board[0][0] != EMPTY_FIELD && board[0][0] === board[0][1] && board[0][1] === board[0][2]) {
-            console.log(board[0][0] + " won");
-            return true;
+            return identifyWinner(board[0][0]);
         } else if (board[1][0] != EMPTY_FIELD && board[1][0] === board[1][1] && board[1][1] === board[1][2]) {
-            console.log(board[1][0] + " won");
-            return true;
+            return identifyWinner(board[1][0]);
         } else if (board[2][0] != EMPTY_FIELD && board[2][0] === board[2][1] && board[2][1] === board[2][2]) {
-            console.log(board[2][0] + " won");
-            return true;
+            return identifyWinner(board[2][0]);
         } else if (board[1][0] != EMPTY_FIELD && board[0][0] === board[1][0] && board[1][0] === board[2][0]) {
-            console.log(board[0][0] + " won");
-            return true;
+            return identifyWinner(board[0][0]);
         } else if (board[0][1] != EMPTY_FIELD && board[0][1] === board[1][1] && board[1][1] === board[2][1]) {
-            console.log(board[0][1] + " won");
-            return true;
+            return identifyWinner(board[0][1]);
         } else if (board[0][2] != EMPTY_FIELD && board[0][2] === board[1][2] && board[1][2] === board[2][2]) {
-            console.log(board[0][2] + " won");
-            return true;
+            return identifyWinner(board[0][2]);
         } else if (board[0][0] != EMPTY_FIELD && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-            console.log(board[0][0] + " won");
-            return true;
+            return identifyWinner(board[0][0]);
         } else if (board[0][2] != EMPTY_FIELD && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-            console.log(board[0][2] + " won");
-            return true;
+            return identifyWinner(board[0][2]);
         } else if (!board[0].includes("") && !board[1].includes("") && !board[2].includes("")) {
-            console.log("tie");
-            return true;
+            return "tie";
         } else {
             return false;
         }
     };
 
     const identifyWinner = (marker) => {
-        // TODO
-        // Return winner based on marker, as passed by gameOver()
-        // WEITER!!!
+        let result;
+
+        players.forEach(player => {
+            if (player.getMarker() === marker) {
+                result = player;
+            }
+        });
+
+        if (result) {
+            return result;
+        } else {
+            throw new Error(`No player with marker '${marker}'`);
+        }
     };
 
     // TODO: Reset game?
@@ -123,15 +134,26 @@ const game = (() => {
 
 // Testing - TODO (later): Remove
 console.log("game.getActivePlayer().getName():", game.getActivePlayer().getName());
+// X win?!
+// game.makeMove(0, 0); // x
+// game.makeMove(1, 0); // o
+// game.makeMove(0, 2); // x
+// game.makeMove(1, 1); // o
+// game.makeMove(0, 1); // x
+// game.makeMove(2, 0); // o
+// game.makeMove(2, 1); // x
+// game.makeMove(1, 2); // o
+// game.makeMove(2, 2); // x
+// Tie
 game.makeMove(0, 0); // x
 game.makeMove(0, 1); // o
 game.makeMove(0, 2); // x
-game.makeMove(1, 1); // o
+game.makeMove(1, 2); // o
 game.makeMove(1, 0); // x
 game.makeMove(2, 0); // o
+game.makeMove(1, 1); // x
+game.makeMove(2, 2); // o
 game.makeMove(2, 1); // x
-game.makeMove(1, 2); // o
-game.makeMove(2, 2); // x
 console.log("game.getActivePlayer().getName():", game.getActivePlayer().getName());
 
 const displayController = (() => {
